b63402c914467e40b80d3e4456da301ce39b49d7
Hunter Reinhart <hreinhartcodes@gmail.com>
2024-09-11 17:29:43 -0500

    Update and rename C/FuckinBianary.c to C/Examples/ToBianary.c

--- C/Examples/ToBianary.c
+++ C/Examples/ToBianary.c
@@ -2,14 +2,14 @@
 
 int main(void) {
 
-int fuck;
+int num;
 int i =0;
-scanf("%d", &fuck);
-   while(i < fuck){
-      if(fuck %2 != 0){
+scanf("%d", &num);
+   while(i < num){
+      if(num %2 != 0){
          printf("1");}
          else{printf("0");}
-         fuck = fuck /2;
+         num = num /2;
    
    }
   
